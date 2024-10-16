0dabad925f4055d7cc85d836f2bb71441b33ffb0
parvejg <parvezpg321@gmail.com>
2024-10-16 20:09:37 +0530

    create a cilent side component

---
+++ app/components/productCard.tsx
@@ -0,0 +1,11 @@
+`use client`;
+import React from 'react'
+
+export const ProductCard = () => {
+  return (
+    <div>
+      <h1>productCard</h1>
+      <button onClick={()=> console.log("add")}>add</button>
+    </div>
+  )
+}
