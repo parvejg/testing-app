78cff80a0797f4db1d15eba47cbc208ee55bc1fd
parvejg <parvezpg321@gmail.com>
2024-10-17 12:49:10 +0530

    add daisyui css into addtocard btn

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export const AddToCart = () => {
   return (
     <div>
-      <h1 className='p-5 my-6 bg-sky-400 text-white text-xl hover:bg-sky-500 text-xl'>AddToCart</h1>
+      <h1 className='btn btn-netutral'>AddToCart</h1>
       {/* <button onClick={()=>console.log("click")}>Add to cart</button> */}
+
     </div>
   )
 }
