8ea2b812d6e04b8b32156c53101d2a9276ba0fe4
parvejg <parvezpg321@gmail.com>
2024-10-17 12:16:13 +0530

    apply tailwind css into addToCart button

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const AddToCart = () => {
   return (
     <div>
-      <h1>AddToCart</h1>
+      <h1 className='p-5 my-6 bg-sky-400 text-white text-xl hover:bg-sky-500 text-xl'>AddToCart</h1>
       {/* <button onClick={()=>console.log("click")}>Add to cart</button> */}
     </div>
   )
