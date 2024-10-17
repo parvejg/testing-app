d15e53861841e7e7b989fbd0a3cd3c846c41a5fd
parvejg <parvezpg321@gmail.com>
2024-10-17 12:50:43 +0530

    add tailwind css

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,12 +1,11 @@
-`use client`;
 import React from 'react'
 import { AddToCart } from './AddToCart';
 
 export const ProductCard = () => {
-  return (
-    <div>
-      <h1>productCard</h1>
+  return (<div>
+      <h1 className='p-5 my-6 bg-sky-400 text-white text-xl hover:bg-sky-500 text-xl'>productCard</h1>
     <AddToCart/>
+    <button className='btn btn-secondary'>Daisy ui btn</button>
     </div>
   )
 }
