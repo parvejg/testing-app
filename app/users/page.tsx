00ef77dd2580f5980c3978b0c4f77e5bc83dc598
parvejg <parvezpg321@gmail.com>
2024-10-17 12:51:12 +0530

    change name

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,7 @@ const UserPage= async()=>{
    const res = await fetch("https://jsonplaceholder.typicode.com/users")
    const users: User[] = await res.json()
    return(<div >
-      <h1>Users </h1>
+      <h1>fetch users data </h1>
       {
          users.map((user)=><li key={user.id}>{user.name}</li>)
       }
